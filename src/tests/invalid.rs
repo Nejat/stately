c99a0a950105a535374ae561a4cce2b6a1cbb4b4
Nejat <nejatoz@gmail.com>
2023-03-26 23:19:04 -0400

    [refactor] clippified invalid fsm builders tests

--- src/tests/invalid.rs
+++ src/tests/invalid.rs
@@ -1,20 +1,21 @@
 #[cfg(not(feature = "nightly"))]
 #[test]
 fn invalid_fsm_builders() {
+    use std::path::Path;
     use trybuild::TestCases;
     use walkdir::WalkDir;
 
     let tester = TestCases::new();
-    let testes = WalkDir::new("src/tests/invalid")
+    let test_cases = WalkDir::new("src/tests/invalid")
         .into_iter()
         // filtering out directories here stops the walker
         .filter_entry(|entry| entry.file_type().is_dir() ||
-            entry.file_name().to_str().map(|entry| entry.ends_with(".rs")).unwrap_or(false))
+            entry.file_name().to_str().map_or(false, |entry| Path::new(entry).extension().map_or(false, |ext| ext.eq_ignore_ascii_case("rs"))))
         // keep only files
-        .filter(|entry| entry.as_ref().map(|entry| entry.file_type().is_file()).unwrap_or(false))
+        .filter(|entry| entry.as_ref().map_or(false, |entry| entry.file_type().is_file()))
         .map(|entry| entry.unwrap().into_path());
 
-    for test in testes {
+    for test in test_cases {
         tester.compile_fail(test);
     }
 }
